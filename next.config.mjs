a8e2b8f1e949d0e917c69b3f4bfec77d0a60af08
T080 <T080@CIPL-ATC00263.colanonline.local>
2025-04-28 18:21:50 +0530

    next config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      // Exclude mysql2 and its dependencies from client-side bundling
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
-  images: {
-    domains: ["cdn.weatherapi.com"], // Add the domain here for external images
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack(config, { isServer }) {
+//     if (!isServer) {
+//       // Exclude mysql2 and its dependencies from client-side bundling
+//       config.resolve.fallback = {
+//         ...config.resolve.fallback,
+//         net: false,
+//         tls: false,
+//       };
+//     }
+//     return config;
+//   },
+//   images: {
+//     domains: ["cdn.weatherapi.com"], // Add the domain here for external images
+//   },
+// };
 
 export default nextConfig;
+
+module.exports = {
+  images: {
+    domains: ["cdn.weatherapi.com"], // add domains where images are hosted
+  },
+};
